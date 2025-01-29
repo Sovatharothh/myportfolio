d70b423428005a1746045b2db9b1d17e571c0905
Sovatharothh <116065704+Sovatharothh@users.noreply.github.com>
2025-01-29 11:31:28 +0700

    add firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-// import roboto fonts
-import "@fontsource/outfit"
-import "@fontsource/roboto"
+// Import Roboto fonts
+import "@fontsource/outfit";
+import "@fontsource/roboto";
 
+// Firebase Initialization
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmq8DBhDvOKspAPpvVCMDpWa3or1z4RH0",
+  authDomain: "rothportfolio-e0ae8.firebaseapp.com",
+  projectId: "rothportfolio-e0ae8",
+  storageBucket: "rothportfolio-e0ae8.firebasestorage.app",
+  messagingSenderId: "969667671215",
+  appId: "1:969667671215:web:b5938b156f37138ef7d05a",
+  measurementId: "G-QS64G86HNP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+console.log("Firebase initialized:", app);
+
+// Render the app
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
